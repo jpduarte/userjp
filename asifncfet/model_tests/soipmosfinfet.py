f92431e55c65512a48c0061c4023432278ab9a82
jpduarte <jpduarte@berkeley.edu>
2015-04-21 14:53:41 -0700

    fit updated for Asif 90nm, Ion lin and sat must be improved, 3 key effects are not present: series resistance, drain saturation, high-k mobility degradation

--- asifncfet/model_tests/soipmosfinfet.py
+++ asifncfet/model_tests/soipmosfinfet.py
@@ -21,9 +21,9 @@ sim1 = pycsimpython.pycsimpython('idvg')
 #add path to verilog code with model, TODO: include case where model is already incorporated to simulator
 sim1.updateparameter('modelpath',rootfolder+'/cmdp/compactmodels/UFCM.py')
 #add path to model card of device under study
-sim1.updateparameter('modelcardpath',rootfolder+'/cmdp/userjp/asifncfet/modelcards/modecardpmosfinfetasif.txt')
+sim1.updateparameter('modelcardpath',rootfolder+'/userjp/asifncfet/modelcards/modecardpmosfinfetasif.txt')
 #add path to folder which will contain simulation files
-sim1.updateparameter('simulationfolder',rootfolder+'/cmdp/userjp/asifncfet/pythonsimulations/idvg/')
+sim1.updateparameter('simulationfolder',rootfolder+'/userjp/asifncfet/pythonsimulations/idvg/')
 #define simulation file name
 sim1.updateparameter('simfilename','pythonsimaux')
 #define simulation final results file 
@@ -37,7 +37,7 @@ sim1.updateparameter('deviceparameter',['Lg'])
 #device parameter values for simulation
 sim1.updateparameter('deviceparametervalue',[[90e-9]])
 #add variables to save  
-sim1.updateparameter('vartosave',['Ids','qd'])#no ok: CBGSI
+sim1.updateparameter('vartosave',['Ids','mu','qd','qs'])#no ok: CBGSI,,'Ft','t','c','mu','mudop','muc','muac','musr'
 
 ###########################################################################
 ##################Simulation Excecution####################################
@@ -47,24 +47,67 @@ Vx = 'Vg'
 
 #plot
 P1 = plotgeneral.plotgeneral()
-P1.updateparameter('ylogflag',1)
-
+P1.updateparameter('ylogflag',0)
 #plot derivative, 1 indicates derivative order
 P1.updateparameter('symbol','o') 
 P1.updateparameter('markersize',8) 
-pathandfile = '/home/juan/research/cmdp/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_50mV.txt'
+pathandfile = rootfolder+'/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_50mV.txt'
 P1.plotfiledata(pathandfile,'Vg','Id',1)
-pathandfile = '/home/juan/research/cmdp/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_500mV.txt'
+pathandfile =  rootfolder+'/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_500mV.txt'
 P1.plotfiledata(pathandfile,'Vg','Id',1)
-pathandfile = '/home/juan/research/cmdp/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_900mV.txt'
+pathandfile =  rootfolder+'/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_900mV.txt'
 P1.plotfiledata(pathandfile,'Vg','Id',1)
 
 pathandfile = sim1.simulationfolder + sim1.simresultfilename
 #plot
 P1.updateparameter('symbol','-') 
 P1.updateparameter('lw',2)
-P1.updateparameter('ylogflag',1)
-
 P1.plotfiledata(pathandfile,Vx,sim1.vartosave[0],1)
+ax = plt.gca()
+ax.set_xlim([-0.9,0])
 
+P1.updateparameter('ylogflag',1)
+#plot derivative, 1 indicates derivative order
+P1.updateparameter('symbol','o') 
+P1.updateparameter('markersize',8) 
+pathandfile = rootfolder+'/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_50mV.txt'
+P1.plotfiledata(pathandfile,'Vg','Id',2)
+pathandfile =  rootfolder+'/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_500mV.txt'
+P1.plotfiledata(pathandfile,'Vg','Id',2)
+pathandfile =  rootfolder+'/userjp/asifncfet/data/ID_VG_NCp90nm_Vdd_900mV.txt'
+P1.plotfiledata(pathandfile,'Vg','Id',2)
+ax = plt.gca()
+ax.set_xlim([-0.9,0])
+
+pathandfile = sim1.simulationfolder + sim1.simresultfilename
+#plot
+P1.updateparameter('symbol','-') 
+P1.updateparameter('lw',2)
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[0],2)
+
+P1.updateparameter('ylogflag',0)
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[1],3)
+
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[2],4)
+P1.updateparameter('symbol','o') 
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[3],4)
+###################################################
+"""
+P1.updateparameter('ylogflag',0)
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[1],3)
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[2],4)
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[3],5)
+P1.plotfiledata(pathandfile,'Ft',sim1.vartosave[4],6)
+
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[4],7)
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[5],7)
+P1.updateparameter('symbol','o') 
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[6],7)
+P1.updateparameter('symbol','s') 
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[7],7)
+P1.updateparameter('symbol','>') 
+P1.plotfiledata(pathandfile,Vx,sim1.vartosave[8],7)
+ax = plt.gca()
+ax.set_ylim([0,35000])
+"""
 plt.show() 
