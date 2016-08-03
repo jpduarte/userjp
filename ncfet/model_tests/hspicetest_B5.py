8550a79cb84b05940df9c472f8e7ed6d089571b4
jpduarte <jpduarte@berkeley.edu>
2016-08-02 23:40:19 -0700

    files for 14nm intel fit with ncfet as well

--- ncfet/model_tests/hspicetest_B5.py
+++ ncfet/model_tests/hspicetest_B5.py
@@ -21,30 +21,42 @@ sim1 = hspicepython.hspicepython('idvg')
 #add path to verilog code with model, TODO: include case where model is already incorporated to simulator
 sim1.updateparameter('modelpath','/users/jpduarte/research/BSIMCMG/code/bsimcmg.va')
 #add path to model card of device under study
-sim1.updateparameter('modelcardpath','/users/jpduarte/research/userjp/ncfet/modelcards/modelcard_nc.nmos')
+sim1.updateparameter('modelcardpath','/users/jpduarte/research/userjp/ncfet/modelcards/internal_14nm.nmos')
 #add path to folder which will contain simulation files
 sim1.updateparameter('simulationfolder',rootfolder+'/userjp/ncfet/hspicesimulations/idvg/')
 #define simulation file name
 sim1.updateparameter('simfilename','hspicesimaux')
 #define simulation final results file 
-sim1.updateparameter('simresultfilename','hspicesimauxresult.txt')
+sim1.updateparameter('simresultfilename','ULPLg14nmFE0nm.txt')
 #include node names in the order defined in verilog code
 sim1.updateparameter('nodes',['Vd', 'Vg', 'Vs', 'Vb'])
 #values for bias conditions of nodes
-sim1.updateparameter('dcbiases',[[0.5],np.concatenate((np.linspace(0,1.0,50),np.linspace(1.0,0,50)),axis=0) , [0], [0]])#np.linspace(0,1.0,100)
+sim1.updateparameter('dcbiases',[[-0.05,-0.7],np.linspace(0,-0.7,100), [0], [0]])#np.linspace(0,1.0,100)
 #device parameters defined to sweep in simulation
-sim1.updateparameter('deviceparameter',['L','alpha1_P','alpha11_P','t_FE',])#,'TFIN','NFIN','NRS','NRD'])#TFIN=15n L=30n NFIN=10 NRS=1 NRD=1
+sim1.updateparameter('deviceparameter',['L','alpha1_P','alpha11_P','t_FE'])#,'TFIN','NFIN','NRS','NRD'])#TFIN=15n L=30n NFIN=10 NRS=1 NRD=1
 #device parameter values for simulation
-sim1.updateparameter('deviceparametervalue',[[30e-9],[-8.4927e+07],[6.1166e+11],[200e-3,500e-9,700e-3]])#,[15e-9],[10],[1],[1]]) 100e-9,200e-9,300e-9
+sim1.updateparameter('deviceparametervalue',[[20e-9],[-32.4927e+08],[24.1166e+12],[0.0e-9]])#,[15e-9],[10],[1],[1]]) 100e-9,200e-9,300e-9
 #add variables to save
-sim1.updateparameter('vartosave',['Ids','qs','vfes','vfed','vgs','QGI'])#,'I1aux','I2aux','I3aux','Ib1aux','Ib2aux','Ib3aux','I4aux','Ib4aux'])
+sim1.updateparameter('vartosave',['Ids','qm','vfes','vfed','vgs','QGI','qms','qmd','qmsguesss','qmsguessd','qmfe1','qmfe2', 'vgn1','vgn2','idsperum'])#
 
 sim1.updateparameter('abstol','1e-6')
 sim1.updateparameter('reltol','1e-6')
-sim1.updateparameter('absv','2e-3')
+sim1.updateparameter('absv','1e-6')
 ###########################################################################
 ##################Simulation Run###########################################
 ###########################################################################
+P1 = plotgeneral.plotgeneral()
+pathandfile ="/users/jpduarte/research/userjp/ncfet/ULP14nmpmos.txt"
+P1.updateparameter('symbol','o')
+P1.updateparameter('lw',2)
+P1.plotfiledata(pathandfile,'Vg','Ids',1)
+
+
+P1.updateparameter('symbol','o')
+P1.updateparameter('lw',2)
+P1.updateparameter('ylogflag',1)
+P1.plotfiledata(pathandfile,'Vg','Ids',2)
+
 sim1.runsim()
 sim1.hspicetotex('x','y')
 #plot
@@ -52,68 +64,46 @@ P1 = plotgeneral.plotgeneral()
 pathandfile = sim1.simulationfolder + sim1.simresultfilename
 P1.updateparameter('symbol','-')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','Ids',1)
+P1.plotfiledata(pathandfile,'Vg','idsperum',1)
+
 
 
 P1.updateparameter('symbol','-')
 P1.updateparameter('lw',2)
 P1.updateparameter('ylogflag',1)
-P1.plotfiledata(pathandfile,'Vg','Ids',2)
+P1.plotfiledata(pathandfile,'Vg','idsperum',2)
+
 
 P1.updateparameter('ylogflag',0)
 P1.updateparameter('symbol','-')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','qs',3)
-
-
-P1.updateparameter('symbol','-')
-P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','vfes',4)
-
-P1.updateparameter('symbol','-')
-P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','vfed',5)
-
-P1.updateparameter('symbol','-')
-P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','QGI',6)
-
-'''P1.updateparameter('symbol','o')
-P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','I1aux',4)
-
-
-P1.updateparameter('symbol','-')
-P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','I2aux',4)
-
-
+P1.plotfiledata(pathandfile,'Vg','qms',3)
 P1.updateparameter('symbol','-.')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','I3aux',4)
+P1.plotfiledata(pathandfile,'Vg','qmsguesss',3)
 
-
-
-P1.updateparameter('symbol','o')
-P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','Ib1aux',5)
-
-
+P1.updateparameter('ylogflag',1)
 P1.updateparameter('symbol','-')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','Ib2aux',5)
-
-
+P1.plotfiledata(pathandfile,'Vg','qms',4)
 P1.updateparameter('symbol','-.')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','Ib3aux',5)
+P1.plotfiledata(pathandfile,'Vg','qmsguesss',4)
 
-P1.updateparameter('symbol','s')
+P1.updateparameter('ylogflag',0)
+P1.updateparameter('symbol','-')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','I4aux',6)
-P1.updateparameter('symbol','o')
+P1.plotfiledata(pathandfile,'Vg','qmd',5)
+P1.updateparameter('symbol','-.')
 P1.updateparameter('lw',2)
-P1.plotfiledata(pathandfile,'Vg','Ib4aux',6)'''
+P1.plotfiledata(pathandfile,'Vg','qmsguessd',5)
 
+P1.updateparameter('ylogflag',1)
+P1.updateparameter('symbol','-')
+P1.updateparameter('lw',2)
+P1.plotfiledata(pathandfile,'Vg','qmd',6)
+P1.updateparameter('symbol','-.')
+P1.updateparameter('lw',2)
+P1.plotfiledata(pathandfile,'Vg','qmsguessd',6)
 
 plt.show() 
